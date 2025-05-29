e6f77157192dbb4b5ba7877d8b79be48c9682b26
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-05-29 17:34:50 +0530

    initial commit

---
+++ Lesson3 Activity2 /activity2.py
@@ -0,0 +1,17 @@
+a= 2010
+print("The type of a is",type(a))
+b= 2010.0321
+print("The type of b is ",type(b))
+c= "2010"
+print("The type of c is", type(c))
+d= False
+print("The type of d is ",type(d))
+
+a1 = str(a)
+print("the type of a1 is ",type(a1))
+b1 = int(b)
+print("The type of b1 is ",type(b1))
+c1 = float(c)
+print("The type of c1 is ", type(c1))
+print(c1) #to understand how to convert string to float 
+
