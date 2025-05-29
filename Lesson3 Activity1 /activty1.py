9b17f3f64a10a2fff459688c1105003c94c654da
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-05-29 17:19:39 +0530

    initial commit

---
+++ Lesson3 Activity1 /activty1.py
@@ -0,0 +1,8 @@
+a=  2010
+print ("The type of a is ", type (a))
+b= 3/2
+print("The type of b is ", type (b))
+c= "201021" 
+print("The type of c is ",type (c))
+d= True
+print("The type of d is ",type(d))
