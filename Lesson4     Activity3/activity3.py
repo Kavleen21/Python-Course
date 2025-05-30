e5616d7e7b6beddb514a1c8af84c9171671bee44
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-05-30 22:14:35 +0530

    initial commit

---
+++ Lesson4     Activity3/activity3.py
@@ -0,0 +1,8 @@
+a= int(input("Marks of English = "))
+b= int(input("Marks of Maths = "))
+c= int(input("Marks of Physics = "))
+d= int(input("Mraks of Chemistry = " ))
+
+percent= ((a+b+c+d)/400)*100
+
+print("The percentage of the four subject = ",percent)
