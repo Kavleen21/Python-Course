57a42ca65879c47c2f69ded7c54965c0b75fcf82
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-05-30 22:08:31 +0530

    initial commit

---
+++ Lesson4   Activity2/activity2.py
@@ -0,0 +1,10 @@
+a= int(input("Enter first amount here : ")) 
+b= a// 100
+c= (a%100)//50
+d= ((a%100)%50)//10
+
+print("Notes of Rs.100 = ",b)
+print("Notes of Rs.50 = ",c)
+print("Notes of Rs.10 = ",d)
+
+
