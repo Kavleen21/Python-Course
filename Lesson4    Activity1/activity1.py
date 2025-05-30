9a18e4111f6fa51a50d823f1350ed417c059fd7c
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-05-30 21:53:47 +0530

    initial commit

---
+++ Lesson4    Activity1/activity1.py
@@ -0,0 +1,9 @@
+a= 94
+b= 98
+c= 41
+d= 95
+e= 11
+avg= (a+b+c+d+e)/5 
+sum= a+b+c+d+e
+print("The sum of all te heights of the trees is :",sum)
+print("The avergae of all the heights of the trees are :",avg)
