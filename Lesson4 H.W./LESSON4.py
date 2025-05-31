6aaf5eae3884fe979ecb8c649d9f6b14d731bc9d
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-05-31 11:51:33 +0530

    initial commit

---
+++ Lesson4 H.W./LESSON4.py
@@ -0,0 +1,9 @@
+a= int(input("Enter you number here for saquare root of a no. : "))
+b= int(input("Enter you number here for square of a no. : "))
+
+x= int(a**0.5)
+y= int(b**2)
+
+
+print("The square root of the number is = ",x)
+print("The square of the number is =  ",y)
