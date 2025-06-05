c0fdfd8d34756592ba7a678d667b6da299fda496
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-05 17:19:11 +0530

    initial commit

---
+++ Lesson5   Activity3/activity3.py
@@ -0,0 +1,9 @@
+a= int(input("Enter your number here : "))
+
+if a>50 : 
+ print("The number is greater than 50")
+ print("It is the if block ")
+
+else:  
+ print("The number is less than 50 ")
+ print ("It is the else block ")
