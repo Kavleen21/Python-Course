c60f7056e61d40167c2dfbe501ab86b3230c35b8
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-05 17:34:19 +0530

    initial commit

---
+++ Lesson5   Activity5/activity5.py
@@ -0,0 +1,9 @@
+a= int(input("Enter the lenght of the first side here : "))
+b= int(input("Enter the length of second side here : "))
+c= int(input("Enter the length of third side here : "))
+
+if a+b>c and a+c>b and b+c>a :
+ print ("The triangle formed will be a valid trangle ")
+
+else: 
+ print("The triangle formed will not be valid ")
