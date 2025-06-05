76e5088147701a1afb7bbb77d77a82449d019df0
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-05 17:12:08 +0530

    initial commit

---
+++ Lesson5.  Acivity2 /activity2.py
@@ -0,0 +1,11 @@
+a= float(input("Enter the selling price here : "))
+b= float(input("Enter the cost price here : "))
+ 
+if a>b :
+ c= a-b 
+
+ print ("The profit is ",c)
+
+else:
+  d= b-a 
+  print ("The loss is ",d)
