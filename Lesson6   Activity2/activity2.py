8ae0716087f80c9cc40fb5b5cbe337dd7ea6fa4c
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-09 17:27:11 +0530

    initial commit

---
+++ Lesson6   Activity2/activity2.py
@@ -0,0 +1,8 @@
+n= int(input("Enter the numerator here : "))
+d= int(input("Enter the denominator here : "))
+
+if n%d == 0 :
+    print(str(n) + " is divisible by " + str(d))
+
+else:
+ print (str(n) + " is not divisible by " + str(d))
