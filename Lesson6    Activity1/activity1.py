535499283363ece9c42fcdb7c0c000dc6587dd17
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-09 17:19:46 +0530

    initial commmit

---
+++ Lesson6    Activity1/activity1.py
@@ -0,0 +1,17 @@
+a= 20 
+b= 98
+c= 74
+d= 21
+e= 0 
+
+e= (a+b)*c/d 
+print("The value of (a+b)*c/d is ",e) 
+
+name= "Kavleen" 
+age= 15
+
+if name == "Kavleen" or name == "John" and age >=2 :
+    print("Hello, Welcome ")
+
+else :
+ print("Goodbye")
