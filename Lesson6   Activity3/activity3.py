5d58fe9f3f7e882cad00ee30f054208e37b3faa7
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-09 17:38:48 +0530

    initial commit

---
+++ Lesson6   Activity3/activity3.py
@@ -0,0 +1,13 @@
+mean1= 38 
+total_no= 40
+correct_no= 56
+wrong_no= 36
+
+sum= mean1*total_no 
+print("The sum of 40 no.s is ",sum)
+
+num1= sum-(wrong_no-correct_no)
+print("sum-(wrong_no-correct_no) is ",num1)
+
+mean2= num1/total_no
+print("The corrected mean is",mean2)
