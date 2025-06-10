276a2b3015a6e58120ecff9265b6b428bc5956bb
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-10 17:02:08 +0530

    initial commit

--- Lesson4     Activity3/activity3.py
+++ Lesson4     Activity3/activity3.py
@@ -1,6 +1,6 @@
 a= int(input("Marks of English = "))
 b= int(input("Marks of Maths = "))
-c= int(input("Marks of Physics = "))
+c= int(input("Marks of Physics= "))
 d= int(input("Mraks of Chemistry = " ))
 
 percent= ((a+b+c+d)/400)*100
---
+++ Lesson6 H.W./LESSON6.py
@@ -0,0 +1,7 @@
+a= int(input("Enter first number here : "))
+b= int(input("Enter second number here : "))
+c= int(input("Enter third number here ; "))
+ 
+print (str(c)+ " is the first number")
+print (str(a)+ " is the second number")
+print (str(b)+ " is the third number")
