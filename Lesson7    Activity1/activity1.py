9fd0a9febbfa15ed0028a40be63fd22f12f033fd
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-16 17:33:16 +0530

    initial commit

---
+++ Lesson7    Activity1/activity1.py
@@ -0,0 +1,13 @@
+a= input("Enter medical condition if any : ")
+b= int(input("Enter the attendence here : "))
+
+if a=="Y":
+    print ("You are eligible for taking the test")
+
+else :
+    if b>=75 :
+     print ("You are eligible for taking the test ")
+
+    else :
+      print ("You are not eligible for taking the test ")
+
