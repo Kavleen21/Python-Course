b4b6591792348874d1de2d511f011f2533d85221
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-17 19:42:33 +0530

    initial commit

---
+++ Lesson8     Activity2/activity2.py
@@ -0,0 +1,9 @@
+a= input("Enter your text here : ")
+
+b= ""            #this is an empty string to store the new string 
+for i in a :
+ b= b + i 
+ print(b)
+
+print("oringinial string ",a)   #original string a 
+print("reversed string ", b)   #reversed string b 
