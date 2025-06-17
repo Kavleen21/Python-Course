4e2b8f1bc7cf5613f17ba2e4033df99da609db8a
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-17 18:33:19 +0530

    initial commit

---
+++ Lesson7 H.W./LESSON7.py
@@ -0,0 +1,7 @@
+a= int(input("Enter your age here : "))
+
+if a>=10 and a<=20 :
+    print ("You are eligible to attend this class ")
+
+else :
+    print ("You are not eligible to attend this class")
