b76d6f9be824931c4e3f6e7dba677c64926b0eb3
Kavleen21 <kaur.kavleen.2010@gmail.com>
2025-06-23 21:25:15 +0530

    initial commit

---
+++ Lesson11    Activity 1/activity1.py
@@ -0,0 +1,9 @@
+print("Half pyramid pattern of stars (*) :")
+a= int(input("Enter the number of rows here : "))
+
+for i in range (a): 
+    
+ for j in range (i+1):
+        print("*", end=" ")
+
+ print()
